4e6905b537bb848b1fef5118962fef409fb3f695
Wojtek <wojciech.smaza@gmail.com>
2018-06-03 21:15:53 +0200

    Add files via upload

--- kurier/script.js
+++ kurier/script.js
@@ -1,5 +1,9 @@
-$(window).on('load resize', function () {
+$(window).on('load resize', function() {
     $('#logo').css({
         'height': $('nav').height() * .9
+    });
+    $('nav ul').css({
+        'margin-left': $('#logo').width(),
+        'width': '100%'- $('nav ul').css('margin-left')
     });
 });
